d924cd604d6a108b3748d3b85dcb9aa8aee0d976
_DevNote <rommelagasa102@gmail.com>
2024-12-23 20:32:19 +0800

    Fix UI

--- MH-TicketingSystem/Controllers/ReportController.cs
+++ MH-TicketingSystem/Controllers/ReportController.cs
@@ -21,59 +21,6 @@ namespace MH_TicketingSystem.Controllers
             return View(reportTickets);
         }
 
-        //public List<ReportTicketViewModel> GetReportTicketViewModel()
-        //{
-        //    var report = _context.Departments
-        //        .GroupJoin(
-        //            _context.Roles,
-        //            department => department.RoleId,
-        //            role => role.Id,
-        //            (department, roles) => new { Department = department, Roles = roles }
-        //        )
-        //        .SelectMany(
-        //            dr => dr.Roles.DefaultIfEmpty(),
-        //            (dr, role) => new { dr.Department, Role = role }
-        //        )
-        //        .GroupJoin(
-        //            _context.UserRoles,
-        //            dr => dr.Role.Id,
-        //            userRole => userRole.RoleId,
-        //            (dr, userRoles) => new { dr.Department, UserRoles = userRoles }
-        //        )
-        //        .SelectMany(
-        //            dru => dru.UserRoles.DefaultIfEmpty(),
-        //            (dru, userRole) => new { dru.Department, UserRole = userRole }
-        //        )
-        //        .GroupJoin(
-        //            _context.Users,
-        //            dru => dru.UserRole.UserId,
-        //            user => user.Id,
-        //            (dru, users) => new { dru.Department, Users = users }
-        //        )
-        //        .SelectMany(
-        //            du => du.Users.DefaultIfEmpty(),
-        //            (du, user) => new { du.Department, User = user }
-        //        )
-        //        .GroupJoin(
-        //            _context.Tickets,
-        //            du => du.User.Id,
-        //            ticket => ticket.UserId,
-        //            (du, tickets) => new { du.Department, Tickets = tickets }
-        //        )
-        //        .AsEnumerable() // Switch to client-side evaluation to handle grouping and counting
-        //        .Select(d => new ReportTicketViewModel
-        //        {
-        //            DepartmentId = d.Department.Id.ToString(),
-        //            Departement = $"{d.Department.DepartmentName} ({d.Department.DepartmentCode})",
-        //            NumberOfTicketClosed = d.Tickets.Count(ticket => ticket.TicketStatus == (int)TicketStatus.Closed),
-        //            NumberOfTicketOpen = d.Tickets.Count(ticket => ticket.TicketStatus == (int)TicketStatus.Open),
-        //            NumberOfPendingTickets = d.Tickets.Count(ticket => ticket.TicketStatus == (int)TicketStatus.Pending)
-        //        })
-        //        .ToList();
-
-        //    return report;
-        //}
-
 
         public List<ReportTicketViewModel> GetReportTicketViewModel()
         {
